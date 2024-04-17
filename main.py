b79ddec33988233c56e4578d5d7b3b02b8b88c14
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-04-17 15:34:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -217,6 +217,8 @@ async def send_text_message(channel_id, visitor_id, message_text, buttons=None):
     async with aiohttp.ClientSession() as session:
         async with session.post(url, data=data, headers=headers) as response:
             response_text = await response.json()
+            print(response_text)
+            print(response.status)
             
             if response.status == 200:
                 try:
