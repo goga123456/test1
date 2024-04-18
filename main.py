7028cc254d5b60fa5f2f7aca2217f8f84092c141
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-04-18 11:08:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -185,9 +185,8 @@ async def route_to_operator(channel_id, visitor_id, group_id=None, operator_id=N
 async def send_text_message(channel_id, visitor_id, message_text, buttons=None):
     url = f'https://bot-api-input.chat.beeline.uz/v1/channel/{channel_id}/visitor/{visitor_id}/text'
     data = {
-        "text": "message_text",
-        "buttons": [],
-        "showInput": "true"
+        "text": message_text,
+        "buttons": []
     }
     headers = {
         "Content-Type": "application/json",
