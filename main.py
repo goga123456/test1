505b3a127408b8f42e5ebc52c0dbda72a0713d96
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-04-18 14:12:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ async def send_text_message(channel_id, visitor_id, message_text, buttons=None):
 
     async with aiohttp.ClientSession() as session:
         async with session.post(url, json=payload, headers=headers) as response:
-            response_text = await response.json()
+            response_text = await response.text()
             print("Response status:", response.status)
             print("Response headers:", response.headers)
             print("Response body:", response.text) 
