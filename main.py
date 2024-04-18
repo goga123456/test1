9477d7840a86e5eba767ed6b1fb89305b06b962b
goga123456 <110159417+goga123456@users.noreply.github.com>
2024-04-18 11:38:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -186,7 +186,8 @@ async def send_text_message(channel_id, visitor_id, message_text, buttons=None):
     url = f'https://bot-api-input.chat.beeline.uz/v1/channel/{channel_id}/visitor/{visitor_id}/text'
     data = {
         "text": message_text,
-        "buttons": []
+        "buttons": [],
+        "showInput": False
     }
     headers = {
         "Content-Type": "application/json",
